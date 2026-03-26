700a00575ac6304e04fe402d02cc0384efb54932
Kee-T <116755980+Kee-T@users.noreply.github.com>
2026-03-26 16:49:37 -0600

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,19 +1,21 @@
 import pandas as pd
+import numpy as np
 
 # i. (set)
-set = pd.Series({'a', 'b', 'c', 'd'})
-set
+# needs to convert set to list first because set is unordered and does not support indexing
+set = pd.Series(list({'a', 'b', 'c', 'd'}))
+print(set)
 
 # ii. (list)
 list = pd.Series(['a', 'b', 'c', 'd'])
-list
+print(list)
 
 # iii. (array)
-array = ({'a', 'b', 'c', 'd'})
+array = np.array(['a', 'b', 'c', 'd'])
 data = pd.Series(array)
-data
+print(data)
 
 # iv. (dictionary)
 # key becomes the index so 0, 1, 2, 3
 dict = pd.Series({0: 'a', 1: 'b', 2: 'c', 3: 'd'})
-dict
+print(dict)
