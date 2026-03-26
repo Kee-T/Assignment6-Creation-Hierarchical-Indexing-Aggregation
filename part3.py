6127be1f1aafa4e36f74a2dcb0b286b6062bf826
Kee-T <116755980+Kee-T@users.noreply.github.com>
2026-03-26 17:00:36 -0600

    Update part3.py

--- part3.py
+++ part3.py
@@ -4,20 +4,20 @@ import pandas as pd
 # needs to convert set to list first because set is unordered and does not support indexing
 S1 = pd.Series(list({'a', 'b', 'c', 'd'}), 
         index=[10, 20, 30, 40])
-S1
+print(S1)
 
 # ii. (list)
 S2 = pd.Series(['e', 'f', 'g', 'h'], 
         index=[10, 20, 30, 40])
-S2
+print(S2)
 
 # iii. (array)
 array = (['i', 'j', 'k', 'l'])
 S3 = pd.Series(array, 
         index = [10, 20, 30, 40])
-S3
+print(S3)
 
 # iv. (dictionary)
 # key becomes the index so 10, 20, 30, 40
 S4 = pd.Series({10: 'm', 20: 'n', 30: 'o', 40: 'p'})
-S4
+print(S4)
