32199d30a21fd01da6ff4f5559ae75dc9174294c
Platypus422 <130885986+Platypus422@users.noreply.github.com>
2026-03-26 22:04:31 -0600

    Update and rename part4.py to part4-9.py

--- part4-9.py
+++ part4-9.py
@@ -1,9 +1,11 @@
+# Part 4a
 DF1 = pd.DataFrame({'S1': S1, 'S2': S2, 'S3': S3, 'S4': S4})
 DF1
 
+# Part 4b
 DF2 - cannot do, unable to guarntee any order for the sets
 
-
+# Part 4c
 DF3 = pd.DataFrame(
     [["a","e","i","m"],
      ["b","f","j","n"],
@@ -15,7 +17,7 @@ DF3 = pd.DataFrame(
 DF3
 
 
-
+# Part 4d
 DF4 = pd.DataFrame(
     np.array([
         ["a","e","i","m"],
@@ -28,7 +30,7 @@ DF4 = pd.DataFrame(
 )
 DF4
 
-
+# Part 4e
 DF5 = pd.DataFrame({
     "S1": {10:"a",20:"b",30:"c",40:"d"},
     "S2": {10:"e",20:"f",30:"g",40:"h"},
@@ -37,6 +39,7 @@ DF5 = pd.DataFrame({
 })
 DF5
 
+# Part 4f
 DF6 = pd.DataFrame([
     {"S1":"a","S2":"e","S3":"i","S4":"m"},
     {"S1":"b","S2":"f","S3":"j","S4":"n"},
@@ -46,7 +49,7 @@ DF6 = pd.DataFrame([
 DF6
 
 
-
+# Part 4g
 DF7 = pd.DataFrame({
     "S1":["a","b","c","d"],
     "S2":["e","f","g","h"],
@@ -59,11 +62,28 @@ DF7
 
 
 
+# Part 5
+DF2 = DF3.copy()
+
+DF_concat = pd.concat([DF1, DF2])
+DF_concat
 
 
 
+# Part 6
+DF_concat.insert(0, "S0", [1,2,2,3,3,4,4,5])
+DF_concat
 
 
+# Part 7
+DF_hier = DF_concat.set_index("S0", append=True)
+DF_hier
+
+# Part 8
+DF_group_index = DF_hier.groupby(level=[0,1]).agg(lambda x: ''.join(x))
+DF_group_index
 
 
-  
+# Part 9
+DF_group_S0 = DF_concat.groupby("S0").agg(lambda x: ''.join(x))
+DF_group_S0
